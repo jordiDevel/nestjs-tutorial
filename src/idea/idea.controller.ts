c60e3adc2cb51351b996f3899b31c02a625e0a06
Jordi Medina <jordi.medinac@gmail.com>
2020-03-12 19:55:05 +0100

    crud operations for idea entity

---
+++ src/idea/idea.controller.ts
@@ -0,0 +1,36 @@
+import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common';
+
+import { IdeaService } from './idea.service';
+import { IdeaDTO } from './idea.dto';
+
+@Controller('idea')
+export class IdeaController {
+    
+    constructor(private ideaService: IdeaService) {}
+
+    @Get()
+    showAllIdeas() {
+        return this.ideaService.showAll();
+    }
+
+    @Post()
+    createIdea(@Body() data: Partial<IdeaDTO>) {
+        return this.ideaService.create(data);
+    }
+
+    @Get(':id')
+    showIdea(@Param('id') id: string) {
+        return this.ideaService.read(id);
+    }
+
+    @Put(':id')
+    updateIdea(@Param('id') id: string, @Body() data: Partial<IdeaDTO>) {
+        return this.ideaService.update(id, data);
+    }
+
+    @Delete(':id')
+    deleteIdea(@Param('id') id: string) {
+        return this.ideaService.delete(id);
+    }
+
+}
